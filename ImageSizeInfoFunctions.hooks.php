31a57d9e52f3ec573a1886c046322262c365bd1e
Tim Aldridge <taldridge@curse.com>
2015-02-25 11:50:06 -0600

    Removed Deprecated class call from static functions.

--- ImageSizeInfoFunctions.hooks.php
+++ ImageSizeInfoFunctions.hooks.php
@@ -37,22 +37,22 @@ class ImageSizeInfoFunctionsHooks {
 	static public function getImageWidth( &$parser, $image = '' ) {
 		try {
 			$title = Title::newFromText( $image, NS_IMAGE );
-			$file = function_exists( 'wfFindFile' ) ? wfFindFile( $title ) : new Image( $title );
+			$file = wfFindFile( $title );
 			$width = ( is_object( $file ) && $file->exists() ) ? $file->getWidth() : 0;
 			return $width;
 		} catch ( Exception $e ) {
-			return wfMessage( 'error' )->text();
+			return wfMessage( 'error_returning_width' )->text();
 		}
 	}
 
 	static public function getImageHeight( &$parser, $image = '' ) {
 		try {
 			$title = Title::newFromText( $image, NS_IMAGE );
-			$file = function_exists( 'wfFindFile' ) ? wfFindFile( $title ) : new Image( $title);
+			$file = wfFindFile( $title );
 			$height = ( is_object( $file ) && $file->exists() ) ? $file->getHeight() : 0;
 			return $height;
 		} catch ( Exception $e ) {
-			return wfMessage( 'error' )->text();
+			return wfMessage( 'error_returning_height' )->text();
 		}
 	}
 }
