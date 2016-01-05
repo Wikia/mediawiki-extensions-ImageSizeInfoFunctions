299246e7679d17cd1343c15108847d53346734c5
Cameron Chunn <cchunn@curse.com>
2016-01-05 16:31:39 -0600

    Updated for MediaWiki 1.25+ Compatibility

--- ImageSizeInfoFunctions.php
+++ ImageSizeInfoFunctions.php
@@ -8,30 +8,15 @@
  * @link		https://github.com/CurseStaff/ImageSizeInfoFunctions
  *
  **/
-/******************************************/
-/* Credits								  */
-/******************************************/
-$credits = [
-	'path'				=>	__FILE__,
-	'name'				=>	'ImageSizeInfoFunctions',
-	'url'				=>	'http://www.mediawiki.org/wiki/Extension:ImageSizeInfoFunctions',
-	'author'			=>	array( 'Dario de Judicibus'),
-	'descriptionmsg'	=>	'imagesizeinfofunctions_description',
-	'version'			=>	'1.1.0'
-];
-$wgExtensionCredits['other'][] = $credits;
-
-/******************************************/
-/* Language Strings, Page Aliases, Hooks  */
-/******************************************/
-$extDir = __DIR__;
-
-$wgExtensionMessagesFiles['ImageSizeInfoFunctions']			= $extDir . "/ImageSizeInfoFunctions.i18n.php";
-$wgExtensionMessagesFiles['ImageSizeInfoFunctionsMagic']	= $extDir . "/ImageSizeInfoFunctions.i18n.magic.php";
-$wgMessagesDir['ImageSizeInfoFunctions']					= $extDir . "/i18n";
-
-$wgAutoloadClasses['ImageSizeInfoFunctionsHooks']			= $extDir . "/ImageSizeInfoFunctions.hooks.php";
-
-$wgHooks['ParserFirstCallInit'][]							= 'ImageSizeInfoFunctionsHooks::onParserFirstCallInit';
-$wgHooks['ParserClearState'][]								= 'ImageSizeInfoFunctionsHooks::onParserClearState';
-
+ if ( function_exists( 'wfLoadExtension' ) ) {
+ 	wfLoadExtension( 'ImageSizeInfoFunctions' );
+ 	// Keep i18n globals so mergeMessageFileList.php doesn't break
+ 	$wgMessagesDirs['ImageSizeInfoFunctions'] = __DIR__ . '/i18n';
+ 	wfWarn(
+ 		'Deprecated PHP entry point used for ImageSizeInfoFunctions extension. Please use wfLoadExtension instead, ' .
+ 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+ 	);
+ 	return;
+ } else {
+ 	die( 'This version of the ImageSizeInfoFunctions extension requires MediaWiki 1.25+' );
+ }
